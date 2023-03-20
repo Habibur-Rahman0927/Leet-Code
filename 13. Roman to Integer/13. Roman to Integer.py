1e539909152ec50ea6e65ff2a453a21eda1fc49e
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-03-20 23:01:51 +0600

    13. Roman to integer solve using for loop

--- 13. Roman to Integer/13. Roman to Integer.py
+++ 13. Roman to Integer/13. Roman to Integer.py
@@ -28,3 +28,30 @@ class Solution(object):
 solution = Solution()
 
 print(solution.romanToInt("MCMXCIV"))
+
+class Solution2(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        lookup = {
+            'I':1,
+            'V':5,
+            'X':10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
+        }
+        output = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and lookup[s[i]] < lookup[s[i + 1]]:
+                output -= lookup[s[i]]
+            else:
+                output += lookup[s[i]]
+        return output
+    
+solution2 = Solution2()
+
+print(solution2.romanToInt("MCMXCIV"))
