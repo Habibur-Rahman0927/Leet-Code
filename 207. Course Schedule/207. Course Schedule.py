940028805e0df5f7fd604c97dacbf29cfb2c8c19
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-05-30 11:14:27 +0600

    207. Course Schedule

--- 207. Course Schedule/207. Course Schedule.py
+++ 207. Course Schedule/207. Course Schedule.py
@@ -1,3 +1,4 @@
+import collections
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
@@ -24,3 +25,20 @@ class Solution(object):
         for crs in range(numCourses):
             if not dfs(crs): return False
         return True
+    
+    def canFinish1(self, numCourses, prerequisites):
+        courses = collections.defaultdict(set)
+        pres = collections.defaultdict(set)
+        for course, pre in prerequisites:
+            courses[course].add(pre)
+            pres[pre].add(course)
+        no_pre_courses = [n for n in range(numCourses) if not courses[n]]
+        count = 0
+        while no_pre_courses:
+            no_pre = no_pre_courses.pop()
+            count+=1
+            for course in pres[no_pre]:
+                courses[course].remove(no_pre)
+                if not courses[course]:
+                    no_pre_courses.append(course)
+        return count == numCourses
