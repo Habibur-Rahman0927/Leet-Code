4aa38e46c4e3488fee8f19a0b91c5f5150199735
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-07-13 10:21:07 +0600

    fixed some issue 36. Valid Sudoku

--- 36. Valid Sudoku/36. Valid Sudoku.py
+++ 36. Valid Sudoku/36. Valid Sudoku.py
@@ -14,11 +14,11 @@ class Solution(object):
                 if board[r][c] == ".":
                     continue
                 if (board[r][c] in rows[r] or 
-                    board[r][c] in cols[r] or
+                    board[r][c] in cols[c] or
                     board[r][c] in squares[(r // 3, c //3)]):
                     return False
                 cols[c].add(board[r][c])
-                rows[c].add(board[r][c])
+                rows[r].add(board[r][c])
                 squares[(r // 3, c //3)].add(board[r][c])
         return True
 
--- 36. Valid Sudoku/README.md
+++ 36. Valid Sudoku/README.md
@@ -2,5 +2,5 @@
 <h3>Medium</h3>
 <hr>
 <div>
-<img alt="" src="./screencapture-leetcode-problems-valid-sudoku-2023-07-13-10_11_58.png" style="width: 100%; height: 100%;">
+<img alt="" src="./screencapture-leetcode-problems-valid-sudoku-2023-07-13-10_19_46.png" style="width: 100%; height: 100%;">
 </div>
