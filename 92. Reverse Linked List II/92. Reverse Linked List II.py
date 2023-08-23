d1d409bde51746b34d65eff8eb564cc27284cc36
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-08-23 10:04:40 +0600

    92. Reverse Linked List II

--- 92. Reverse Linked List II/92. Reverse Linked List II.py
+++ 92. Reverse Linked List II/92. Reverse Linked List II.py
@@ -11,3 +11,18 @@ class Solution(object):
         :type right: int
         :rtype: ListNode
         """
+        dummy = ListNode(0, head)
+
+        leftPrev, cur = dummy, head
+        for i in range(left - 1):
+            leftPrev, cur = cur, cur.next
+        
+        prev = None
+        for i in range(right - left + 1):
+            tmpNext = cur.next
+            cur.next = prev
+            prev, cur = cur, tmpNext
+        
+        leftPrev.next.next = cur
+        leftPrev.next = prev
+        return dummy.next
