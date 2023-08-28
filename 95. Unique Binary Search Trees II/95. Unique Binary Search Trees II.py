b79241840cfcc4bca01a83c9c10b9b6ba25ac422
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-08-28 09:42:01 +0600

    95. Unique Binary Search Trees II

--- 95. Unique Binary Search Trees II/95. Unique Binary Search Trees II.py
+++ 95. Unique Binary Search Trees II/95. Unique Binary Search Trees II.py
@@ -10,3 +10,17 @@ class Solution(object):
         :type n: int
         :rtype: List[TreeNode]
         """
+        def generate(left, right):
+            if left == right:
+                return [TreeNode(left)]
+            if left > right:
+                return [None]
+            
+            res = []
+            for val in range(left, right + 1):
+                for leftTree in generate(left, val - 1):
+                    for rightTree in generate(val + 1, right):
+                        root = TreeNode(val, leftTree, rightTree)
+                        res.append(root)
+            return res
+        return generate(1, n)
