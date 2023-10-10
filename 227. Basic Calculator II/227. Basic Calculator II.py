2bbe3c0f20b705ad2c9e58a63583fd7605bb0726
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2023-10-11 00:03:16 +0600

    227. Basic Calculator II

--- 227. Basic Calculator II/227. Basic Calculator II.py
+++ 227. Basic Calculator II/227. Basic Calculator II.py
@@ -1,26 +1,44 @@
 class Solution:
   def calculate(self, s: str) -> int:
-    ans = 0
-    prevNum = 0
-    currNum = 0
-    op = '+'
-
-    for i, c in enumerate(s):
-      if c.isdigit():
-        currNum = currNum * 10 + int(c)
-      if not c.isdigit() and c != ' ' or i == len(s) - 1:
-        if op == '+' or op == '-':
-          ans += prevNum
-          prevNum = currNum if op == '+' else -currNum
-        elif op == '*':
-          prevNum = prevNum * currNum
-        elif op == '/':
-          if prevNum < 0:
-            prevNum = math.ceil(prevNum / currNum)
-          else:
-            prevNum = prevNum // currNum
-        op = c
-        currNum = 0
+        num_stack = []
+        num = 0
+        op = '+'
+        for s_s in s:
+            if s_s == ' ':
+                continue
+            elif s_s.isdigit():
+                num = num * 10 + int(s_s)
+            else:
+                if op == '*':
+                    x = num_stack.pop()
+                    num_stack.append(x * num)
+                elif op == '/':
+                    x = num_stack.pop()
+                    if x < 0:
+                        num_stack.append(-((-x) / num))
+                    else:
+                        num_stack.append(x / num)
+                elif op == '-':
+                    num_stack.append(-1 * num)
+                else:
+                    num_stack.append(num)
+                op = s_s
+                num = 0
+        else:
+            if op == '*':
+                x = num_stack.pop()
+                num_stack.append(x * num)
+            elif op == '/':
+                x = num_stack.pop()
+                if x < 0:
+                    num_stack.append(-((-x)/ num))
+                else:
+                    num_stack.append(x / num)
+            elif op == '-':
+                num_stack.append(-1 * num)
+            else:
+                num_stack.append(num)
+        return sum(num_stack)
 
 solution = Solution()
 print(solution.calculate("3+2*2"))
--- 227. Basic Calculator II/README.md
+++ 227. Basic Calculator II/README.md
@@ -2,5 +2,5 @@
 <h3>Medium</h3>
 <hr>
 <div>
-<img alt="" src="./screencapture-leetcode-problems-basic-calculator-ii-2023-10-10-23_39_27.png" style="width: 100%; height: 100%;">
+<img alt="" src="./screencapture-leetcode-problems-basic-calculator-ii-2023-10-10-23_54_39.png" style="width: 100%; height: 100%;">
 </div>
