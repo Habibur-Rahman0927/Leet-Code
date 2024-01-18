46c81e2a4aacea0ca800acdefaf2b1dc59e60798
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2024-01-18 12:21:46 +0600

    70. Climbing Stairs another solutions

--- 70. Climbing Stairs/70. Climbing Stairs.py
+++ 70. Climbing Stairs/70. Climbing Stairs.py
@@ -4,11 +4,18 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [0, 1, 2]
-        if 2 < n:
-            for i in range(3, n + 1):
-                dp.append(dp[i - 2] + dp[i - 1])
-        return dp[n]
+        # dp = [0, 1, 2]
+        # if 2 < n:
+        #     for i in range(3, n + 1):
+        #         dp.append(dp[i - 2] + dp[i - 1])
+        # return dp[n]
+
+        one, two = 1, 1
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
     
 solution = Solution()
-print(solution.climbStairs(3))
+print(solution.climbStairs(5))
