49e533ae43d5857373e15a41ea9a979bdef4a89e
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2024-02-02 17:29:06 +0600

    1291. Sequential Digits

---
+++ 1291. Sequential Digits/1291. Sequential Digits.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def sequentialDigits(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: List[int]
+        """
+        a = []
+
+        for i in range(1, 10):
+            num = i
+            next_digit = i + 1
+
+            while num <= high and next_digit <= 9:
+                num = num * 10 + next_digit
+                if low <= num <= high:
+                    a.append(num)
+                next_digit += 1
+
+        a.sort()
+        return a
