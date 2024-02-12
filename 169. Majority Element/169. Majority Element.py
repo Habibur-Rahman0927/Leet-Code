6f5f94f90acc97b6f9c56a05254690e0b986fc29
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2024-02-12 10:24:37 +0600

    169. Majority Element

--- 169. Majority Element/169. Majority Element.py
+++ 169. Majority Element/169. Majority Element.py
@@ -1,21 +1,25 @@
+from collections import Counter
 class Solution(object):
     def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        candidate = 0
-        counter = 0
-        for val in nums:
-            if counter == 0:
-                candidate = val
+        # candidate = 0
+        # counter = 0
+        # for val in nums:
+        #     if counter == 0:
+        #         candidate = val
 
-            if candidate == val:
-                counter += 1
-            else:
-                counter -= 1
+        #     if candidate == val:
+        #         counter += 1
+        #     else:
+        #         counter -= 1
 
-        return candidate
+        # return candidate
+        count = Counter(nums)
+        return max(count, key=count.get)
+
     
 solution = Solution()
 print(solution.majorityElement([2,2,1,1,1,2,2]))
