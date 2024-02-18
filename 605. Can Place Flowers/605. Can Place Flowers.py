761821f7dec98e6bee99a198d7b6e4a3945bc3a8
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2024-02-18 11:51:53 +0600

    605. Can Place Flowers

--- 605. Can Place Flowers/605. Can Place Flowers.py
+++ 605. Can Place Flowers/605. Can Place Flowers.py
@@ -5,10 +5,23 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        f = [0] + flowerbed + [0]
+        # f = [0] + flowerbed + [0]
 
-        for i in range(1, len(f) - 1):
-            if f[i - 1] == 0 and f[i] == 0 and f [i + 1] == 0:
-                f[i] = 1
-                n -= 1
-        return n <= 0
+        # for i in range(1, len(f) - 1):
+        #     if f[i - 1] == 0 and f[i] == 0 and f [i + 1] == 0:
+        #         f[i] = 1
+        #         n -= 1
+        # return n <= 0
+
+        i = 0
+        cnt = 0
+        while i < len(flowerbed):
+            if (flowerbed[i] == 0 and (i <= 0 or flowerbed[i - 1] == 0) and (i >= len(flowerbed) - 1 or flowerbed[i + 1] == 0)):
+                cnt += 1
+                i += 1
+            i += 1
+        return n <= cnt
+
+solution = Solution()
+solution.canPlaceFlowers([1,0,0,0,1], 1)
+
