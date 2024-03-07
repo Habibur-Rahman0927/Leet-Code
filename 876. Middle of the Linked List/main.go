daf27e3ee7d01025a961cd638aef9284f258b858
Habibur-Rahman0927 <habibur.rahman.0927@gmail.com>
2024-03-07 20:41:32 +0600

    876. Middle of the Linked List

---
+++ 876. Middle of the Linked List/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func main(){
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	head.Next.Next.Next = &ListNode{Val: 4}
+	head.Next.Next.Next.Next = &ListNode{Val: 5}
+
+	fmt.Println(middleNode(head).Val)
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
